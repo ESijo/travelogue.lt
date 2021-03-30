c5ffff64c6ee824f7607430e1b974eed91cb5ecc
Egle <egle.sidaraviciute@gmail.com>
2021-03-30 15:47:12 +0300

    update Services css

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,8 +2,8 @@ function Services() {
     return (
         <div className="services-container">
             <h1>Ką mes veikiam?</h1>
-            <div className="services-column">
-                <div>
+            <div className="services-row">
+                <div className="services-column">
                     <div>
                         <h2>Kuriame maršrutus</h2>
                         <p>Kuriame unikalius, alternatyvius maršrutus po Lietuvą. Juos galite įsigyti mūsų el.parudotuvėje ar gauti dovanų kas mėnesį tapę mūsų patreonais.</p>
@@ -15,9 +15,9 @@ function Services() {
                 </div>
                 <img src="https://static.wixstatic.com/media/cf462c_caa794b7c69046f89fbcc2323885547f~mv2.jpg/v1/fill/w_802,h_1190,al_c,q_85,usm_0.66_1.00_0.01/IMG_4671.webp" alt=""></img>
             </div>
-            <div className="services-column">
+            <div className="services-row">
                 <img src="https://static.wixstatic.com/media/cf462c_e9720d17e1ba486bb6c9da8a9e8fd0d2~mv2.jpg/v1/fill/w_774,h_1138,al_c,q_85,usm_0.66_1.00_0.01/IMG_4557.webp" alt=""></img>
-                <div>
+                <div className="services-column">
                      <div>
                         <h2>Kuriame turinį</h2>
                         <p>Travelogue LT - mūsų portfolio. Esame tekstų kūrėjos ir fotografės duetas, tad jei patinka, ką darome, galite kreiptis į mus dėl turinio kūrimo ir socialinių tinklų koncepcijos bei strategijos savo verslui.</p>
